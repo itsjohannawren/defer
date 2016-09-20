cde5e43e9a2cfd761dc92364e6c5e1d12462a59a
Jeff Walter <jeffwalter@users.noreply.github.com>
2016-09-19 23:35:08 -0700

    What was I thinking?

--- examples/close.c
+++ examples/close.c
@@ -1,6 +1,6 @@
 // http://github.com/jeffwalter/defer
 //
-// Copyright <c) 2016 Jeff Walter <jeff@404ster.com>
+// Copyright (c) 2016 Jeff Walter <jeff@404ster.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
